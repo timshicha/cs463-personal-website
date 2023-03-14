4cb715f56c63b695df24a36f49cdbd032c0f765b
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-03-14 05:26:48 -0700

    narrow screen now gets navbar as a collapsible menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import HamburgerMenuImg from '../assets/hamburger-menu.png';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
+  const [menuExpanded, setMenuExpanded] = useState(false);
+
   return (
     <>
       <nav className="nav-bar">
@@ -26,6 +30,42 @@ const Navbar = () => {
               Contact
             </Link>
           </div>
+        </div>
+
+        <div className="menu-div">
+          <button
+            type="button"
+            className="nav-menu-btn"
+            onClick={() => setMenuExpanded(!menuExpanded)}
+          >
+            <img
+              src={HamburgerMenuImg}
+              alt="Expand the navigation bar"
+              className="menu-icon"
+            />
+          </button>
+          <ul className={'expanded-menu ' + (menuExpanded ? '' : 'hidden')}>
+            <li className="menu-item">
+              <Link to="/" className="menu-link">
+                About Me
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/previous-work" className="menu-link">
+                Previous Work
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/projects" className="menu-link">
+                Projects
+              </Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/contact" className="menu-link">
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
